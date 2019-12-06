d8d3acbfc54f1897ce88c8fbdf56316d7bfe8866
Sandra Tsao <sandratsao@Sandras-MacBook-Pro.local>
2019-12-06 00:59:35 -0500

    complete data

--- routes/index.js
+++ routes/index.js
@@ -46,6 +46,18 @@ router.get("/condata/oceania", (req, res) => {
         res.json(result[0]); // send that row back to the calling function
     })
 })
+router.get("/condata/oceania2", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_oceania WHERE ID = "2"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
 
 router.get("/condata/other", (req, res) => {
     //here is where we set up the quiery
@@ -73,6 +85,45 @@ router.get("/condata/america", (req, res) => {
     })
 })
 
+router.get("/condata/america2", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_america WHERE ID = "2"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/america3", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_america WHERE ID = "3"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/america4", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_america WHERE ID = "4"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
 router.get("/condata/europe", (req, res) => {
     //here is where we set up the quiery
     let query = `SELECT * FROM tbl_europe`;
@@ -86,9 +137,217 @@ router.get("/condata/europe", (req, res) => {
     })
 })
 
+router.get("/condata/europe2", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_europe WHERE ID = "2"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/europe3", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_europe WHERE ID = "3"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/europe4", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_europe WHERE ID = "4"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/europe5", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_europe WHERE ID = "5"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/europe6", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_europe WHERE ID = "6"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/europe7", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_europe WHERE ID = "7"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
 router.get("/condata/asia", (req, res) => {
     //here is where we set up the quiery
     let query = `SELECT * FROM tbl_asia`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia2", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "2"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia3", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "3"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia4", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "4"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia5", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "5"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia6", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "6"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia7", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "7"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia8", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "8"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia9", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "9"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia10", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "10"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { console.log(err); } //something broke!
+
+        console.log(result); //this should be the database row
+
+        res.json(result[0]); // send that row back to the calling function
+    })
+})
+
+router.get("/condata/asia11", (req, res) => {
+    //here is where we set up the quiery
+    let query = `SELECT * FROM tbl_asia WHERE ID = "11"`;
 
     sql.query(query, (err, result) => {
         if (err) { console.log(err); } //something broke!
